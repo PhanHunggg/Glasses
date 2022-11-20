f265b63d8634f687ad1fff1749f27129ba355d8c
PhanHung <phanquochung280702@gmail.com>
2022-11-20 21:09:30 +0700

    fix

--- js/main.js
+++ js/main.js
@@ -102,7 +102,7 @@ const setGlasses = () => {
   let html = dataGlasses.reduce((total, element) => {
     total += `
         <div class="col-4">
-        <button ><img class="img-fluid ${element.id}" src="${element.src}" alt=""></img></button>
+        <button onclick="handleGlass('${element.id}')" ><img class="img-fluid ${element.id}" src="${element.src}" alt=""></img></button>
             </div>
         `;
     return total;
@@ -111,148 +111,19 @@ const setGlasses = () => {
 };
 setGlasses();
 
-// const handlePlus = (id) => {
-//   for(let i in dataGlasses){
-//     if(dataGlasses[i].id == id){
-//       document.getElementById(id).innerHTML = `
-//       <img class="img-fluid" src="${dataGlasses[i].virtualImg}" alt=""></img>`
+
+// const handleGlass = (get) => {
+//   console.log(this);
+
+//   for (let i in dataGlasses) {
+//     if (get === dataGlasses[i].id) {
+//       document.getElementById("avatar").innerHTML = `
+//             <img src="${dataGlasses[i].virtualImg}" alt=""></img>
+//           `;
 //     }
 //   }
 // };
 
+// lí do viết phần xuát hiện kính bằng script bên html vì khi viết bên js hàm handleGlass như ở trên thì nó không chạy đc và bị lỗi : Uncaught ReferenceError: handleGlass is not defined at HTMLButtonElement.onclick
+// Em đã tìm cách fix nhưng khong đc nên đành viết kia. 
 
-document.querySelectorAll(".img-fluid")[0].addEventListener("click", (e) => {
-  for (let i in dataGlasses) {
-    if (e.path[0].classList[1] === dataGlasses[i].id) {
-      document.getElementById("avatar").innerHTML = `
-      <img src="${dataGlasses[i].virtualImg}" alt=""></img>
-      `;
-      document.getElementById("glassesInfo").style.display = "block";
-      document.getElementById("glassesInfo").innerHTML = `
-      <h5>${dataGlasses[i].name} - ${dataGlasses[i].brand} ( ${dataGlasses[i].color})</h5>
-      <p><span>${dataGlasses[i].price}</span> Stocking</p>
-      <p>${dataGlasses[i].description}</p>
-      `
-    }
-  }
-});
-document.querySelectorAll(".img-fluid")[1].addEventListener("click", (e) => {
-  for (let i in dataGlasses) {
-    if (e.path[0].classList[1] === dataGlasses[i].id) {
-      document.getElementById("avatar").innerHTML = `
-      <img src="${dataGlasses[i].virtualImg}" alt=""></img>
-      `;
-      document.getElementById("glassesInfo").style.display = "block";
-      document.getElementById("glassesInfo").innerHTML = `
-      <h5>${dataGlasses[i].name} - ${dataGlasses[i].brand} ( ${dataGlasses[i].color})</h5>
-      <p><span>${dataGlasses[i].price}</span> Stocking</p>
-      <p>${dataGlasses[i].description}</p>
-      `
-    }
-  }
-});
-document.querySelectorAll(".img-fluid")[2].addEventListener("click", (e) => {
-  for (let i in dataGlasses) {
-    if (e.path[0].classList[1] === dataGlasses[i].id) {
-      document.getElementById("avatar").innerHTML = `
-      <img src="${dataGlasses[i].virtualImg}" alt=""></img>
-      `;
-      document.getElementById("glassesInfo").style.display = "block";
-      document.getElementById("glassesInfo").innerHTML = `
-      <h5>${dataGlasses[i].name} - ${dataGlasses[i].brand} ( ${dataGlasses[i].color})</h5>
-      <p><span>${dataGlasses[i].price}</span> Stocking</p>
-      <p>${dataGlasses[i].description}</p>
-      `
-    }
-  }
-});
-document.querySelectorAll(".img-fluid")[3].addEventListener("click", (e) => {
-  for (let i in dataGlasses) {
-    if (e.path[0].classList[1] === dataGlasses[i].id) {
-      document.getElementById("avatar").innerHTML = `
-      <img src="${dataGlasses[i].virtualImg}" alt=""></img>
-      `;
-      document.getElementById("glassesInfo").style.display = "block";
-      document.getElementById("glassesInfo").innerHTML = `
-      <h5>${dataGlasses[i].name} - ${dataGlasses[i].brand} ( ${dataGlasses[i].color})</h5>
-      <p><span>${dataGlasses[i].price}</span> Stocking</p>
-      <p>${dataGlasses[i].description}</p>
-      `
-    }
-  }
-});
-document.querySelectorAll(".img-fluid")[4].addEventListener("click", (e) => {
-  for (let i in dataGlasses) {
-    if (e.path[0].classList[1] === dataGlasses[i].id) {
-      document.getElementById("avatar").innerHTML = `
-      <img src="${dataGlasses[i].virtualImg}" alt=""></img>
-      `;
-      document.getElementById("glassesInfo").style.display = "block";
-      document.getElementById("glassesInfo").innerHTML = `
-      <h5>${dataGlasses[i].name} - ${dataGlasses[i].brand} ( ${dataGlasses[i].color})</h5>
-      <p><span>${dataGlasses[i].price}</span> Stocking</p>
-      <p>${dataGlasses[i].description}</p>
-      `
-    }
-  }
-});
-document.querySelectorAll(".img-fluid")[5].addEventListener("click", (e) => {
-  for (let i in dataGlasses) {
-    if (e.path[0].classList[1] === dataGlasses[i].id) {
-      document.getElementById("avatar").innerHTML = `
-      <img src="${dataGlasses[i].virtualImg}" alt=""></img>
-      `;
-      document.getElementById("glassesInfo").style.display = "block";
-      document.getElementById("glassesInfo").innerHTML = `
-      <h5>${dataGlasses[i].name} - ${dataGlasses[i].brand} ( ${dataGlasses[i].color})</h5>
-      <p><span>${dataGlasses[i].price}</span> Stocking</p>
-      <p>${dataGlasses[i].description}</p>
-      `
-    }
-  }
-});
-document.querySelectorAll(".img-fluid")[6].addEventListener("click", (e) => {
-  for (let i in dataGlasses) {
-    if (e.path[0].classList[1] === dataGlasses[i].id) {
-      document.getElementById("avatar").innerHTML = `
-      <img src="${dataGlasses[i].virtualImg}" alt=""></img>
-      `;
-      document.getElementById("glassesInfo").style.display = "block";
-      document.getElementById("glassesInfo").innerHTML = `
-      <h5>${dataGlasses[i].name} - ${dataGlasses[i].brand} ( ${dataGlasses[i].color})</h5>
-      <p><span>${dataGlasses[i].price}</span> Stocking</p>
-      <p>${dataGlasses[i].description}</p>
-      `
-    }
-  }
-});
-document.querySelectorAll(".img-fluid")[7].addEventListener("click", (e) => {
-  for (let i in dataGlasses) {
-    if (e.path[0].classList[1] === dataGlasses[i].id) {
-      document.getElementById("avatar").innerHTML = `
-      <img src="${dataGlasses[i].virtualImg}" alt=""></img>
-      `;
-      document.getElementById("glassesInfo").style.display = "block";
-      document.getElementById("glassesInfo").innerHTML = `
-      <h5>${dataGlasses[i].name} - ${dataGlasses[i].brand} ( ${dataGlasses[i].color})</h5>
-      <p><span>${dataGlasses[i].price}</span> Stocking</p>
-      <p>${dataGlasses[i].description}</p>
-      `
-    }
-  }
-});
-document.querySelectorAll(".img-fluid")[8].addEventListener("click", (e) => {
-  for (let i in dataGlasses) {
-    if (e.path[0].classList[1] === dataGlasses[i].id) {
-      document.getElementById("avatar").innerHTML = `
-      <img src="${dataGlasses[i].virtualImg}" alt=""></img>
-      `;
-      document.getElementById("glassesInfo").style.display = "block";
-      document.getElementById("glassesInfo").innerHTML = `
-      <h5>${dataGlasses[i].name} - ${dataGlasses[i].brand} ( ${dataGlasses[i].color})</h5>
-      <p><span>${dataGlasses[i].price}</span> Stocking</p>
-      <p>${dataGlasses[i].description}</p>
-      `
-    }
-  }
-});
